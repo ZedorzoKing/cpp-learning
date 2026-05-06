4e84356fbb5e471bc6a7fb8bf3529b52f47dea0b
Oliver <4e.cox.oliver@gmail.com>
2026-05-06 19:31:24 +0200

    Add funciones exercise

---
+++ 01-fundamentos/04_funciones.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int pedirNumeroPositivo(int num) {
+    while (!(num >= 0))
+    {
+        cout <<"Numero invalido. Intentalo de nuevo: ";
+        cin >> num;
+    }
+    
+    return num;
+}
+
+int calcularSuma(int num) {
+    
+    int suma = 0;
+
+    for (int i = 0; i <= num; i++)
+    {
+        suma += i;
+    }
+    
+    return suma;
+}
+
+int contarPares(int num) {
+    int pares =0;
+
+    for (int i = 1; i <= num; i++)
+    {
+        if (i % 2 == 0)
+        {
+            pares++;
+        }
+    }
+    
+    return pares;
+}
+
+int main() {
+
+    int num;
+    int impares;
+    int pares;
+    int suma;
+
+
+    cout << "Introduce un numero entero positivo: ";
+    cin >> num;
+    num = pedirNumeroPositivo(num);
+    suma = calcularSuma(num);
+    pares = contarPares(num);
+    impares = num - pares;
+
+    for (int i = 1; i <= num; i++)
+    {
+        cout << "Numero: " << i << ".\n";
+    }
+    
+
+    cout << "suma total: " << suma << ".\n";
+    cout << "Cantidad de pares: " << pares << ".\n";
+    cout << "Cantidad de impares: " << impares << ".\n";
+
+    return 0;
+}
